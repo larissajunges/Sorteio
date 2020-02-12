5aa61ca0f4c02ef1d6a16ebb870fda2953cb8d53
aluno <adsroot.aluno.ifsc.edu.br>
2020-02-11 22:28:55 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -0,0 +1,24 @@
+package br.ifsc.edu.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mudarTexto(View view){
+        TextView v = findViewById(R.id.test);
+        v.setText("Finishing");
+
+    }
+}
