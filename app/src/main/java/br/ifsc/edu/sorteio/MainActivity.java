515e125145ae8963d196420080f2a8b944989f04
aluno <adsroot.aluno.ifsc.edu.br>
2020-02-18 21:19:44 -0300

    exemplo 1

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -1,19 +1,39 @@
 package br.ifsc.edu.sorteio;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView tv;
+        EditText editText;
+        Button button;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycleActivity","OnCreate");
+        tv = findViewById(R.id.textViewResultado);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(editText.getText().toString());
+            }
+        });
     }
 
     public void mudarTexto(View view){
@@ -21,4 +41,25 @@ public class MainActivity extends AppCompatActivity {
         v.setText("Finishing");
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Resume", "OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause", "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("onRestart", "onRestart");
+
+    }
+
+
 }
